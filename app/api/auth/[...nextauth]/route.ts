c5fc119b1703d9cae07d6d145d933793b89d8b0a
MohammedThalhaA <mohammedthalha177@gmail.com>
2026-09-25 16:20:56 +0530

    feat: add NextAuth API route handler

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,13 +2,6 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { NextRequest } from "next/server";
 
-const handler = async (req: NextRequest, ctx: any) => {
-  const host = req.headers.get("host");
-  if (host) {
-    const protocol = process.env.NODE_ENV === "development" ? "http" : "https";
-    process.env.NEXTAUTH_URL = `${protocol}://${host}`;
-  }
-  return NextAuth(authOptions)(req, ctx);
-};
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
